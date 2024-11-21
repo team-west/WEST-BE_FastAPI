d2f54e36ecb301cd998312e899e8a6ed26e7c372
김대운 <eodnseodns23@gmail.com>
2024-11-22 07:04:29 +0900

    mainpage weather

--- user/crud/user_crud.py
+++ user/crud/user_crud.py
@@ -6,3 +6,80 @@ from user.schemas import schemas
 
 def get_user_by_id(user_id: str, db: Session = Depends(get_db)):
   return db.query(models.User).filter(models.User.id == user_id).first()
+
+import requests
+import json
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+API_KEY = os.getenv("API_KEY")
+
+def get_weather(latitude, longitude):
+  url = f"https://api.openweathermap.org/data/2.5/forecast?lat={latitude}&lon={longitude}&appid={API_KEY}"
+
+  response = requests.get(url)
+  if response == 400:
+    return 400
+  elif response == 401:
+    return 401
+  elif response == 403:
+    return 403
+  elif response == 404:
+    return 404
+  elif response == 500:
+    return 500
+  contexts = response.text
+  contents = json.loads(contexts)
+  city = contents['city']['name']
+  if city == "Daejeon":
+    city = '대전'
+  elif city == "Seoul":
+    city = '서울'
+  elif city == "Sejong":
+    city = '세종'
+  elif city == "Gwacheon":
+    city = "과천"
+  elif city == "Gwangmyeong":
+    city = "광명"
+  else:
+    city = "대한민국"
+
+  weather = contents['list'][2]['weather'][0]['main']
+  if weather == "Clear":
+    weather = '맑음'
+  elif weather == "Rain":
+    weather = '비'
+  elif weather == "Clouds":
+    weather = '구름'
+  else:
+    weather = '맑음'
+
+  weather_detail = contents['list'][2]['weather'][0]['description']
+  if weather_detail == "light rain":
+    weather_detail = '가벼운 비'
+  elif weather_detail == "moderate rain":
+    weather_detail = "적당한 비"
+  elif weather_detail == "scattered clouds":
+    weather_detail = '흩어진 구름'
+  elif weather_detail == "few clouds":
+    weather_detail = "구름 거의 없음"
+  elif weather_detail == "broken clouds":
+    weather_detail = ''
+  elif weather_detail == "overcast clouds":
+    weather_detail = '빽빽한 구름'
+  elif weather_detail == "clear sky":
+    weather_detail = "맑은 하늘"
+  else:
+    weather_detail = "맑은 하늘"
+
+  temperature = contents['list'][2]['main']['temp']
+  temperature = temperature - 270.15
+
+  temperature_min = contents['list'][2]['main']['temp_min']
+  temperature_min = temperature_min - 270.15
+
+  temperature_max = contents['list'][2]['main']['temp_max']
+  temperature_max = temperature_max - 270.15
+
+  return {'city': city, 'weather': weather, 'weather_detail': weather_detail, 'temp': temperature, 'temp_min': temperature_min, 'temp_max': temperature_max}
--- user/routes/router.py
+++ user/routes/router.py
@@ -3,15 +3,29 @@ from sqlalchemy.orm import Session
 from user.database.databases import get_db
 from user.model.models import User
 from user.database import databases
-from user.crud.user_crud import get_user_by_email
+from user.crud.user_crud import get_user_by_id, get_weather
 router = APIRouter(
   prefix='/user'
 )
 
 @router.get('/mypage')
-async def get_mypage(user_id: str, db: Session = Depends(get_db)):
+async def mypage(user_id: str, db: Session = Depends(get_db)):
   user = get_user_by_id(user_id=user_id, db=db)
   if not user:
     HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="USER NOT FOUND.")
   print(user)
   return user
+
+@router.get('/main')
+async def mainpage(latitude: float, longitude: float): #, db: Session = Depends(get_db)):
+  weather = get_weather(latitude, longitude)
+  if weather == 400:
+    raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Bad Request For OpenAPI.")
+  elif weather == 401:
+    raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Don't Have Authorized Token for OpenAPI.")
+  elif weather == 403:
+    raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Unauthorized API Token Used for OpenAPI.")
+  elif weather == 500:
+    raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="OpenAPI SERVER Error.")
+
+  return weather
